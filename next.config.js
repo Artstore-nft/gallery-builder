d8a419e28046d3cdca0429013b59459b8d725cff
madhawk1296 <mac@macs-iMac.local>
2022-03-19 17:52:51 -0600

    added akamai image loader

--- next.config.js
+++ next.config.js
@@ -6,9 +6,15 @@ module.exports = {
     MORALIS_API_KEY: process.env.MORALIS_API_KEY
   },
   images: {
+    loader: 'akamai',
+    path: '',
+  },
+  /*
+  images: {
     domains: ['ipfs.moralis.io', 'lh3.googleusercontent.com', 'pbs.twimg.com', 'ikzttp.mypinata.cloud', 
     'images.0xzuki.com', 'prod-metadata.s3.amazonaws.com', 'duskbreakers.gg', 'i6gznw767phrf5tnpxtdo4pxmuhame3lmtxwomfhwl5mys2dnkaa.arweave.net',
     'wearables.sfo2.digitaloceanspaces.com']
   },
+  */
   reactStrictMode: false,
 }
