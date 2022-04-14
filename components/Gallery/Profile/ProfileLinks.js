e41ac66e49b42bce86dba04ff775a58a4601d3cf
madhawk1296 <mac@macs-iMac.local>
2022-04-14 14:52:21 -0600

    added linktypes to ProfileLinks

--- components/Gallery/Profile/ProfileLinks.js
+++ components/Gallery/Profile/ProfileLinks.js
@@ -1,5 +1,3 @@
-import linkTypes from '../../../data/linktypes'
-
 // import hooks
 import { createRef, useCallback, useContext, useEffect, useRef, useState } from 'react'
 import { useAppDispatch } from "../../../app/hooks"
@@ -10,6 +8,73 @@ import {
 } from "../../../features/settings/settingsSlice"
 
 import { ProfileContext } from './Profile'
+
+
+const linkTypes = [
+    {
+        id: "foundation",
+        label: "Foundation",
+        display: "@",
+        location: "https://foundation.app/@",
+        logo: "/images/logos/foundation.svg",
+    },
+    {
+        id: "opensea",
+        label: "OpenSea",
+        display: "",
+        location: "https://opensea.io/",
+        logo: "/images/logos/opensea.svg",
+    },
+    {
+        id: "superrare",
+        label: "SuperRare",
+        display: "@",
+        location: "https://superrare.com/",
+        logo: "/images/logos/superrare.png",
+    },
+    {
+        id: "rarible",
+        label: "Rarible",
+        display: "",
+        location: "https://rarible.com/",
+        logo: "/images/logos/rarible.svg",
+    },
+    {
+        id: "etherscan",
+        label: "Etherscan",
+        display: "",
+        location: "https://etherscan.io/address/",
+        logo: "/images/logos/etherscan.svg",
+    },
+    {
+        id: "facebook",
+        label: "Facebook",
+        display: "",
+        location: "https://facebook.com/",
+        logo: "/images/logos/facebook.png",
+    },
+    {
+        id: "instagram",
+        label: "Instagram",
+        display: "@",
+        location: "https://www.instagram.com/",
+        logo: "/images/logos/instagram.png",
+    },
+    {
+        id: "twitter",
+        label: "Twitter",
+        display: "@",
+        location: "https://twitter.com/",
+        logo: "/images/logos/twitter.svg",
+    },
+    {
+        id: "other",
+        label: "Other",
+        display: "",
+        location: "",
+        logo: "/images/icons/website.svg",
+    },
+]
 
 export default function ProfileLinks() {
     const { profile, previewMode } = useContext(ProfileContext) || null
